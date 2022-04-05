25bfc34ffd50c13e169452efbfc520f829172bf8
Gilberto Guerrero <gguerrero@innovaschools.edu.co>
2022-04-04 22:29:51 -0500

    Update README and Update app and folders

--- coding/api-app/app.js
+++ coding/api-app/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'api app',
                 // location: ret.data.trim()
             })
         }
